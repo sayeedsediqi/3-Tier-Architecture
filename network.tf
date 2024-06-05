e058a8735ec2869076a0e9857831c5cdb560b224
Sayeed Mohammad <60190007+sayeedsediqi@users.noreply.github.com>
2024-06-04 22:30:14 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -33,9 +33,43 @@ resource "azurerm_private_dns_zone" "sql_private_dns" {
   resource_group_name = var.resource_group_name
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
-  name                  = "dns-link"
+resource "azurerm_private_dns_zone" "webapp_private_dns" {
+  name                = "privatelink.azurewebsites.net"
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "sql_dns_link" {
+  name                  = "sql-dns-link"
   resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.sql_private_dns.name
   virtual_network_id    = azurerm_virtual_network.spoke_vnet.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "webapp_dns_link" {
+  name                  = "webapp-dns-link"
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.webapp_private_dns.name
+  virtual_network_id    = azurerm_virtual_network.spoke_vnet.id
+}
+
+resource "azurerm_subnet" "subnet_firewall" {
+  name                 = "AzureFirewallSubnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.hub_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet_sql" {
+  name                 = "subnet-sql"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.spoke_vnet.name
+  address_prefixes     = ["10.1.1.0/24"]
+  service_endpoints    = ["Microsoft.Sql"]
+}
+
+resource "azurerm_subnet" "subnet_webapp" {
+  name                 = "subnet-webapp"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.spoke_vnet.name
+  address_prefixes     = ["10.1.2.0/24"]
 }
